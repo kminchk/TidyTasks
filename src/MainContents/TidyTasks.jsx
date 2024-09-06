6c5c0709847cdcd2cb60b5271b9707006994ab05
mindev <minchk01@gmail.com>
2024-09-06 19:51:37 +0700

    Refactor CSS and add functionality to TidyTasks component

--- src/MainContents/TidyTasks.jsx
+++ src/MainContents/TidyTasks.jsx
@@ -1,65 +1,100 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 function TidyTasks() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+
+  // Function to save tasks to localStorage
+  const saveTasksToLocalStorage = (tasks) => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    console.log("Tasks saved to localStorage! : ", tasks);
+  };
+
+  // Add a new task
   const addTask = () => {
-    if (newTask.trim() !== "") {
-      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
+    if (newTask.trim()) {
+      const updatedTasks = [
+        ...tasks,
+        { id: Date.now(), text: newTask, completed: false },
+      ];
+      setTasks(updatedTasks);
       setNewTask("");
+      saveTasksToLocalStorage(updatedTasks);
+      console.log("New task added!");
     }
   };
 
+  // Toggle the completed status of a task
   const toggleTask = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
     );
+    setTasks(updatedTasks);
+    saveTasksToLocalStorage(updatedTasks);
   };
 
+  // Delete a task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+    saveTasksToLocalStorage(updatedTasks);
+    console.log("Task deleted!");
   };
+
+  // Load tasks from localStorage when component mounts
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
 
   return (
-    <div>
-      <div>
-        {tasks.map((task) => (
-          <div
-            key={task.id}
-            className={`card ${task.completed ? "bg-base-200" : ""}`}
-          >
-            <div className="flex justify-center items-center">
-              <input
-                type="checkbox"
-                className="form-checkbox"
-                checked={task.completed}
-                onChange={() => toggleTask(task.id)}
-              />
-              <p className="ml-2">{task.text}</p>
-              <RemoveCircleIcon
-                sx={{ ml: 2, color: "#e74c3c" }}
-                onClick={() => deleteTask(task.id)}
-              />
-            </div>
+    <div className="p-4 flex flex-col">
+      <div className="flex-grow overflow-hidden">
+        <div className="h-[calc(65vh-4rem)] overflow-y-auto p-2 rounded">
+          <div className="space-y-2">
+            {tasks.map((task) => (
+              <div
+                key={task.id}
+                className="flex items-center p-2 rounded bg-base-200 text-2xl"
+              >
+                <p
+                  className={`flex-grow cursor-pointer text-white ${
+                    task.completed ? "line-through decoration-red-500" : ""
+                  }`}
+                  onClick={() => toggleTask(task.id)}
+                >
+                  {task.text}
+                </p>
+                <button
+                  onClick={() => deleteTask(task.id)}
+                  className="text-red-500 hover:text-red-700 ml-2"
+                >
+                  <RemoveCircleIcon />
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex justify-center items-center gap-3">
-        <input
-          type="text"
-          placeholder="Add Task"
-          className="w-1/3 input input-bordered mr-2"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          onKeyPress={(e) => e.key === "Enter" && addTask()}
-        />
-        <button className="btn btn-circle btn-error" onClick={addTask}>
-          <AddIcon />
-        </button>
+        </div>
+        <div className="flex items-center mt-4">
+          <input
+            type="text"
+            placeholder="Add Task"
+            className="input input-bordered flex-grow mr-2"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && addTask()}
+          />
+          <button
+            className="btn-circle bg-gray-100 text-red-500 p-2"
+            onClick={addTask}
+          >
+            <AddIcon />
+          </button>
+        </div>
       </div>
     </div>
   );
